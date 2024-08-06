83efddfb185d246bbe906ffb3208b7ac03204125
zakaria.narjis <zakaria.narjis.97@gmail.com>
2024-08-06 02:29:35 +0200

    reformatt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -19,14 +19,16 @@ release = "0.1.2"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
 
 # autodoc_default_options = {
 #     "members": True,
@@ -38,7 +40,7 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 master_doc = "index"
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
 # autodoc_mock_imports = [
 #     'modularl.agents.sac.SAC',
 #     'modularl.agents.td3.TD3',
@@ -50,5 +52,5 @@ autodoc_member_order = 'bysource'
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-html_logo = '_static/modulaRL_logo.svg'
+html_logo = "_static/modulaRL_logo.svg"
 # Add the project root to the system path
