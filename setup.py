a834f9e45224826abbe9817f2fb900c16972ef82
zakaria.narjis <zakaria.narjis.97@gmail.com>
2024-08-07 16:20:30 +0200

    update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="modularl",
-    version="0.1.2",
+    version="0.1.3",
     author="Zakaria Narjis",
     author_email="zakaria.narjis.97@gmail.com",
     description="A modular reinforcement learning library",
